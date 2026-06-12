1dd2de8486c3a131d35b88041a0ad1d4ed2f8c52
Saltysnacc124 <operator3450@gmail.com>
2026-06-12 12:02:44 +0530

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.orm import declarative_base
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -9,3 +10,16 @@ class Machine(Base):
     id = Column(Integer, primary_key=True, index=True)
     machine_name = Column(String(50))
     machine_type = Column(String(50))
+
+class MachineEvent(Base):
+    __tablename__ = "machine_events"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    machine_name = Column(String(50))
+    status = Column(String(20))
+
+    temperature = Column(Float)
+    production_count = Column(Integer)
+
+    timestamp = Column(DateTime, default=datetime.utcnow)

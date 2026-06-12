b85410877bf2aa64fca2e650e2844d86a78a0dcc
Saltysnacc124 <operator3450@gmail.com>
2026-06-12 13:08:49 +0530

    Update crud.py

--- database/crud.py
+++ database/crud.py
@@ -1,1 +1,15 @@
+from models import MachineEvent
 
+def create_machine_event(db, machine_name, status, temperature, production_count):
+    event = MachineEvent(
+        machine_name=machine_name,
+        status=status,
+        temperature=temperature,
+        production_count=production_count
+    )
+
+    db.add(event)
+    db.commit()
+    db.refresh(event)
+
+    return event

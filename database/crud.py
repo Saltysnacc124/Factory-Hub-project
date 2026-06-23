d186b522cfc1bab83dd6cd12eafa5f5a447a138e
Saltysnacc124 <operator3450@gmail.com>
2026-06-23 11:40:12 +0530

    Update crud.py

--- database/crud.py
+++ database/crud.py
@@ -1,11 +1,46 @@
 from models import MachineEvent
 
-def create_machine_event(db, machine_name, status, temperature, production_count):
+
+def create_machine_event(
+    db,
+    machine_name,
+    status,
+    spindle_rpm,
+    feed_rate,
+    spindle_load,
+    spindle_temp,
+    x_position,
+    y_position,
+    z_position,
+    tool_id,
+    tool_wear,
+    current_cycle_time,
+    cycle_duration,
+    production_count,
+    alarm_code,
+    alarm_severity,
+    alarm_message,
+    event_type
+):
     event = MachineEvent(
         machine_name=machine_name,
         status=status,
-        temperature=temperature,
-        production_count=production_count
+        spindle_rpm=spindle_rpm,
+        feed_rate=feed_rate,
+        spindle_load=spindle_load,
+        spindle_temp=spindle_temp,
+        x_position=x_position,
+        y_position=y_position,
+        z_position=z_position,
+        tool_id=tool_id,
+        tool_wear=tool_wear,
+        current_cycle_time=current_cycle_time,
+        cycle_duration=cycle_duration,
+        production_count=production_count,
+        alarm_code=alarm_code,
+        alarm_severity=alarm_severity,
+        alarm_message=alarm_message,
+        event_type=event_type
     )
 
     db.add(event)
